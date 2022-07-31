07218d0fb99766b1ad3fa8767d8fbef075ab05db
abdoaligomaa <abdoaligomaa107@gmail.com>
2022-07-31 18:33:00 +0200

    finish assignment three

--- week one/dist/index.js
+++ week one/dist/index.js
@@ -12,10 +12,25 @@
     5- in ts config file => src map => using for debuging and testing
     
 */
+/*
 // first function using typescript
-function calculate(numOne, numTwo) {
-    return numOne + numTwo;
+assignment two
+function calculate(numOne:number,numTwo:number):number {
+  return numOne + numTwo;
 }
+
 console.log(calculate(10, 20)); // 30
-console.log(calculate("10", "20")); // We Don't Need This To Work
+// console.log(calculate("10", "20")); // We Don't Need This To Work
 console.log(calculate(+true, +true)); // 2
+
+*/
+/*
+// assignment three
+function printInfo(valueOne:number|string, valueTwo:number|string):string {
+  return `Value One Is ${valueOne}, Value Two Is ${valueTwo}`;
+}
+
+console.log(printInfo(10, 20)); // Value One Is 10, Value Two Is 20
+console.log(printInfo("10", "20")); // Value One Is "10", Value Two Is "20"
+console.log(printInfo(true, [1, 2, 3])); // We Don't Need This To Work
+*/
