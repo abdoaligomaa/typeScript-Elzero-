6c0f9f62ec3946b2cd65b77b83672fac2e65a910
abdoaligomaa <abdoaligomaa107@gmail.com>
2022-07-31 18:23:40 +0200

    add types to function

--- week one/dist/index.js
+++ week one/dist/index.js
@@ -16,6 +16,6 @@
 function calculate(numOne, numTwo) {
     return numOne + numTwo;
 }
-// console.log(calculate(10, 20)); // 30
-// console.log(calculate("10", "20")); // We Don't Need This To Work
-// console.log(calculate(+true, +true)); // 2
+console.log(calculate(10, 20)); // 30
+console.log(calculate("10", "20")); // We Don't Need This To Work
+console.log(calculate(+true, +true)); // 2
