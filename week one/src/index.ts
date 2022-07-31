33d93ceacb0e35c666559b831087155a8d12c369
abdoaligomaa <abdoaligomaa107@gmail.com>
2022-07-31 17:48:24 +0200

    init config file

---
+++ week one/src/index.ts
@@ -0,0 +1,11 @@
+/*
+    1- use command tsc --int to create tsc config file
+
+    2- in ts config file => root dir => the path in which i put ts files in    
+    ./src
+
+    3- in ts config file => out dir => the path in which typeScipt generate     javaScipt code ./dist
+
+    4-
+*/
+
