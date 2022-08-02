100baa22b25ead5e1381e6d4eb3c84c1e006fa70
abdoaligomaa <abdoaligomaa107@gmail.com>
2022-08-02 22:13:42 +0200

    solving assingment two

---
+++ week two/dist/assignment2.js
@@ -0,0 +1,6 @@
+"use strict";
+// Do Not Edit Here
+let myInfo;
+myInfo = 1000; // No Problem Here
+myInfo = true; // No Problem Here
+console.log(myInfo);
---
+++ week two/src/assignment2.ts
@@ -0,0 +1,7 @@
+// Write Your Code Here
+type mix=boolean|number
+// Do Not Edit Here
+let myInfo: mix;
+myInfo = 1000; // No Problem Here
+myInfo = true; // No Problem Here
+console.log(myInfo);
